387e69ea9d68644ddec9742e71ad8d495ba557ca
ryanshaw31 <rdshaw31@gmail.com>
2026-03-04 13:40:40 -0500

    load csv

--- load.py
+++ load.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+data = pd.read_csv('bike_data.csv')
+pd.set_option("display.max_columns", None) # allows all columns to be viewed
+
+station_ids = data['start.station.id'].unique().tolist()
+sorted_station_ids = sorted(station_ids)
+station_names = data['start.station.name'].unique().tolist()
+
+stations = dict(zip(sorted_station_ids, station_names))
